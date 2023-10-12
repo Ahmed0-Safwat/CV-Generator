ac64ec9bc2c6d20097cbe1736cdf0c286b4f4c92
Ahmed0-Safwat <ahmed.sm.lasheen@gmail.com>
2023-10-12 14:16:02 +0300

    profile changed

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Typography, Stack, Divider, Box } from "@mui/material";
-import HowItWorks from "../../components/Home/HowItWorks/HowItWorks";
-import OurValues from "../../components/Home/OurValues/OurValues";
 
 const Profile = () => {
   return (
@@ -9,27 +7,72 @@ const Profile = () => {
       direction="row"
       sx={{
         transform: "translateY(90px)",
-        mb: 10,
+        mb: 16,
+        mt: 4,
+        ml: 4,
+        mr: 4,
       }}
     >
-      <Stack width="100%">
-        <Typography variant="h4">Profile</Typography>
+      <Stack
+        width="30%"
+        height="100%"
+        sx={{
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <img style={{ width: "50%" }} src="/images/profile.jpg" alt="" />
 
-        <Stack
-          direction="row"
-          style={{ alignItems: "center", height: "100px" }}
+        <Typography
+          align="center"
+          fontSize="30px"
+          lineHeight="52px"
+          fontWeight={600}
+          className="how-it-works-title"
         >
-          <Divider
-            orientation="vertical"
-            sx={{ bgcolor: "primary.main", mx: 2 }}
-          />
-        </Stack>
+          Salma Rashad
+        </Typography>
 
-        <HowItWorks />
-        <OurValues />
+        <Typography
+          align="center"
+          fontSize="24px"
+          lineHeight="52px"
+          fontWeight={400}
+          className="how-it-works-title"
+        >
+          SalmaRashad61@yahoo.com
+        </Typography>
       </Stack>
 
-      <Stack width="100%">new stuff</Stack>
+      <Stack
+        width="100%"
+        spacing={4}
+        sx={{
+          padding: "16px",
+        }}
+      >
+        <Typography
+          align="center"
+          fontSize="50px"
+          lineHeight="52px"
+          fontWeight={600}
+          className="how-it-works-title"
+          sx={{
+            fontFamily: "Pacifico",
+            textDecorationLine: "underline",
+          }}
+        >
+          Profile
+        </Typography>
+
+        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
+        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
+        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
+        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
+        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
+
+
+      </Stack>
     </Stack>
   );
 };
