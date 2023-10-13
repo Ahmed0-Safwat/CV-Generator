020447ba22bd61c19d32c478e78eb2a7ff4f777c
Ahmed0-Safwat <ahmed.sm.lasheen@gmail.com>
2023-10-13 17:57:47 +0300

    profile page changed

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,47 +1,81 @@
 import React from "react";
-import { Typography, Stack, Divider, Box } from "@mui/material";
+import { Typography, Stack } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 const Profile = () => {
   return (
     <Stack
-      direction="row"
       sx={{
         transform: "translateY(90px)",
         mb: 16,
-        mt: 4,
-        ml: 4,
-        mr: 4,
       }}
     >
       <Stack
-        width="30%"
-        height="100%"
-        sx={{
-          alignItems: "center",
-          textAlign: "center",
+        style={{
+          backgroundImage: "url(/images/hero-image.png)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          borderRadius: "0 0 30px 30px",
         }}
       >
-        <img style={{ width: "50%" }} src="/images/profile.jpg" alt="" />
-
-        <Typography
-          align="center"
-          fontSize="30px"
-          lineHeight="52px"
-          fontWeight={600}
-          className="how-it-works-title"
+        <Stack
+          sx={{
+            ml: { xs: 0, md: 0, lg: 4 },
+            alignItems: {
+              xs: "center",
+              md: "center",
+              lg: "flex-start",
+            },
+            textAlign: {
+              xs: "center",
+              lg: "left",
+            },
+          }}
         >
-          Salma Rashad
-        </Typography>
+          <Stack
+            width="30%"
+            height="100%"
+            sx={{
+              alignItems: { xs: "center", lg: "center " },
+              textAlign: "center",
+              mt: 2,
+              mb: 2,
+              ml: 2,
+            }}
+          >
+            <img
+              style={{ width: "50%", borderRadius: "50%" }}
+              src="/images/profile.jpg"
+              alt=""
+            />
 
-        <Typography
-          align="center"
-          fontSize="24px"
-          lineHeight="52px"
-          fontWeight={400}
-          className="how-it-works-title"
-        >
-          SalmaRashad61@yahoo.com
-        </Typography>
+            <Typography
+              align="center"
+              fontWeight={600}
+              lineHeight="35px"
+              className="how-it-works-title"
+              sx={{
+                color: "white",
+                fontSize: { xs: "14px", md: "24px", lg: "32px" },
+              }}
+            >
+              Ahmed Safwat
+            </Typography>
+
+            <Typography
+              align="center"
+              fontWeight={200}
+              className="how-it-works-title"
+              sx={{
+                color: "white",
+                fontSize: { xs: "10px", md: "24px", lg: "30px" },
+              }}
+            >
+              ahmedsafwat11@yahoo.com
+            </Typography>
+          </Stack>
+        </Stack>
       </Stack>
 
       <Stack
@@ -62,16 +96,53 @@ const Profile = () => {
             textDecorationLine: "underline",
           }}
         >
-          Profile
+          Saved Cv
         </Typography>
+        <Stack
+          gap={2}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <img
+            style={{
+              width: "400px",
+              height: "400px",
+              borderRadius: "20px",
+            }}
+            src=" /images/hero-image.png"
+          />
+          <img
+            style={{
+              width: "400px",
 
-        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
-        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
-        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
-        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
-        <a style={{color:"#36C"}} href="">Http/https://wave.video/convert/youtube-to-mp4-260</a>
+              height: "400px",
+              borderRadius: "20px",
+            }}
+            src=" /images/hero-image.png"
+          />
+          <img
+            style={{
+              width: "400px",
 
+              height: "400px",
+              borderRadius: "20px",
+            }}
+            src=" /images/hero-image.png"
+          />
+          <img
+            style={{
+              width: "400px",
 
+              height: "400px",
+              borderRadius: "20px",
+            }}
+            src=" /images/hero-image.png"
+          />
+        </Stack>
       </Stack>
     </Stack>
   );
